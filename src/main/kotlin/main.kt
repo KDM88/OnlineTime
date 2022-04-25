1d7f2b2505ea77b17949cc2c1303cb9f6e913217
KDM88 <karpova.dm@me.com>
2022-04-25 12:07:48 +0300

    update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,3 @@
-const  val TIME_ONLINE = 86_500
-const val MINUTES = TIME_ONLINE / 60
-const val HOURS = TIME_ONLINE / 3600
 const val HOUR_ENDING1 = "час"
 const val HOUR_ENDING2 = "часа"
 const val HOUR_ENDING3 = "часов"
@@ -9,17 +6,21 @@ const val MINUTE_ENDING2 = "минуты"
 const val MINUTE_ENDING3 = "минут"
 
 fun main() {
-    agoToText(TIME_ONLINE)
+    agoToText(timeOnline, minutes, hours)
 }
 
-fun agoToText (TIME_ONLINE : Int) {
-    when (TIME_ONLINE) {
+val timeOnline = 1_500
+val minutes = timeOnline / 60
+val hours = timeOnline / 3600
+
+fun agoToText(timeOnline: Int, minutes: Int, hours: Int) {
+    when (timeOnline) {
         in 0..60 -> println("был(а) только что")
         in 61..3_600 -> {
-            println("был(а) $MINUTES $timeMinute назад ")
+            println("был(а) $minutes $timeMinute назад ")
         }
         in 3_601..86_400 -> {
-            println("был(а) $HOURS $timeHour назад")
+            println("был(а) $hours $timeHour назад")
         }
         in 86_400..172_800 -> println("был(а) сегодня")
         in 172_801..259_200 -> println("был(а) вчера")
@@ -28,13 +29,13 @@ fun agoToText (TIME_ONLINE : Int) {
 }
 
 val timeMinute = when {
-     ((MINUTES % 10 == 1) && (MINUTES % 10 != 11)) -> MINUTE_ENDING1
-     ((MINUTES % 10 == 2) || (MINUTES % 10 == 3) || (MINUTES % 10 == 4)) -> MINUTE_ENDING2
-     else -> MINUTE_ENDING3
+    ((minutes % 10 == 1) && (minutes % 10 != 11)) -> MINUTE_ENDING1
+    ((minutes % 10 == 2) || (minutes % 10 == 3) || (minutes % 10 == 4)) -> MINUTE_ENDING2
+    else -> MINUTE_ENDING3
 }
 
 val timeHour = when {
-    ((HOURS % 10 == 1) && (HOURS % 10 != 11)) -> HOUR_ENDING1
-    ((HOURS % 10 == 2) || (HOURS % 10 == 3) || (HOURS % 10 == 4)) -> HOUR_ENDING2
+    ((hours % 10 == 1) && (hours % 10 != 11)) -> HOUR_ENDING1
+    ((hours % 10 == 2) || (hours % 10 == 3) || (hours % 10 == 4)) -> HOUR_ENDING2
     else -> HOUR_ENDING3
 }
